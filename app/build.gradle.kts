7aa6266f98772c3ba322d5ab1f750d0bbd7aa197
hazem khaled <hazemk778@gmail.com>
2023-09-07 18:04:36 +0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,127 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.hazem.boruto"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.hazem.boruto"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    kotlin {
+        jvmToolchain(8)
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //navigation in compose
+    val navigationVersion = "2.7.2"
+    implementation("androidx.navigation:navigation-compose:$navigationVersion")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+
+    //room
+    val roomVersion = "2.5.2"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    //paging
+    implementation("androidx.paging:paging-compose:3.2.1")
+    //serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    //data store preferences
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.46")
+    ksp("com.google.dagger:hilt-android-compiler:2.44")
+    //coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    //horizontal pager and indicator
+    implementation ("com.google.accompanist:accompanist-pager:0.33.1-alpha")
+    implementation ("com.google.accompanist:accompanist-pager-indicators:0.33.1-alpha")
+    //swipe to refresh accompanist
+    implementation ("com.google.accompanist:accompanist-swiperefresh:0.33.1-alpha")
+    //palette Api
+    implementation("androidx.palette:palette-ktx:1.0.0")
+    //system ui accompanist
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.33.1-alpha")
+
+    //testing
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.2")
+    androidTestImplementation( "androidx.compose.ui:ui-test-junit4:1.5.1")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.5.1")
+
+    testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.jetbrains.kotlin:kotlin-test-junit :1.9.10")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
+
+}
