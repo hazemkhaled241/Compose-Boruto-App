a86679ae141bfe52032e3c695c285148aaa58672
hazem khaled <hazemk778@gmail.com>
2023-09-12 00:04:03 +0300

    create file for all dimensions

---
+++ app/src/main/java/com/hazem/boruto/presentation/ui/theme/Dimens.kt
@@ -0,0 +1,11 @@
+package com.hazem.boruto.presentation.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val EXTRA_LARGE_PADDING=40.dp
+val LARGE_PADDING=20.dp
+val MEDIUM_PADDING=16.dp
+val SMALL_LARGE_PADDING=10.dp
+val VERY_SMALL_LARGE_PADDING=4.dp
+val PAGER_INDICATOR_HEIGHT=50.dp
+val FINISH_BUTTON_WIDTH=150.dp
