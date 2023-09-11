973cb6bfae9a6daa287844556e56878f6fff5b2e
hazem khaled <hazemk778@gmail.com>
2023-09-12 00:02:33 +0300

    add new colors

--- app/src/main/java/com/hazem/boruto/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/hazem/boruto/presentation/ui/theme/Color.kt
@@ -1,5 +1,8 @@
 package com.hazem.boruto.presentation.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,7 +12,23 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-val purple200= Color(0xFFBB86FC)
-val purple500= Color(0xFF6200EE)
-val purple700= Color(0xFF3700B3)
-val Teal200= Color(0XFF03DAC5)
+val purple200 = Color(0xFFBB86FC)
+val purple500 = Color(0xFF6200EE)
+val purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0XFF03DAC5)
+
+
+val LightGray = Color(0XFFD8D8D8)
+val DarkGray = Color(0XFF2A2A2A)
+
+val Colors.welcomeScreenBackGroundColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) Color.Black  else Color.White
+
+val Colors.titleColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) LightGray else DarkGray
+
+val Colors.descriptionColor
+    @Composable
+    get() = if (isSystemInDarkTheme()) LightGray.copy(alpha = .5f) else DarkGray.copy(alpha = .5f)
