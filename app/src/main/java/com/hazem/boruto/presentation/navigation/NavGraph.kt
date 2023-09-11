5b5e4f1459917fcc4a16c8cfefd1265cad3d9595
hazem khaled <hazemk778@gmail.com>
2023-09-12 00:03:22 +0300

    update the nav graph

--- app/src/main/java/com/hazem/boruto/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/hazem/boruto/presentation/navigation/NavGraph.kt
@@ -7,11 +7,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.hazem.boruto.presentation.screens.splash.SplashScreen
+import com.hazem.boruto.presentation.screens.welcome.WelcomeScreen
 import com.hazem.boruto.utils.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
 fun SetUpNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Screen.Splash.route){
+    NavHost(navController = navController, startDestination = Screen.Welcome.route){
         composable(route=Screen.Splash.route){
             SplashScreen(navController)
         }
@@ -21,7 +22,9 @@ fun SetUpNavGraph(navController: NavHostController) {
         })){
 
         }
-        composable(route=Screen.Welcome.route){}
+        composable(route=Screen.Welcome.route){
+            WelcomeScreen(navController = navController)
+        }
         composable(route=Screen.Search.route){}
     }
 }
