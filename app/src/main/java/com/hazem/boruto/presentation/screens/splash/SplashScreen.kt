e7697f2d6008016239e201fccc1302920e96913b
hazem khaled <hazemk778@gmail.com>
2023-09-11 16:10:47 +0300

    animate the icon of splash screen

--- app/src/main/java/com/hazem/boruto/presentation/screens/splash/SplashScreen.kt
+++ app/src/main/java/com/hazem/boruto/presentation/screens/splash/SplashScreen.kt
@@ -1,13 +1,18 @@
 package com.hazem.boruto.presentation.screens.splash
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -20,18 +25,27 @@ import com.hazem.boruto.presentation.ui.theme.purple700
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
+    val degree= remember { Animatable(0f) }
+    LaunchedEffect(key1 = true ){
+        degree.animateTo(360f, animationSpec = tween(
+            durationMillis = 1000,
+            delayMillis = 200
+        ))
+    }
+    Splash(degree = degree.value)
 }
 
 @Composable
-fun Splash() {
+fun Splash(degree:Float) {
     if(isSystemInDarkTheme()){
         Box(
             modifier = Modifier
                 .background(Color.Black)
                 .fillMaxSize()
         ) {
-            Image(modifier = Modifier.align(Alignment.Center), painter = painterResource(id = R.drawable.logo), contentDescription = stringResource(R.string.app_logo) )
+            Image(modifier = Modifier
+                .align(Alignment.Center)
+                .rotate(degree), painter = painterResource(id = R.drawable.logo), contentDescription = stringResource(R.string.app_logo) )
         }
     }
     else{
@@ -40,12 +54,14 @@ fun Splash() {
             .background(Brush.verticalGradient(listOf(purple700, purple500)))
             .fillMaxSize()
     ) {
-        Image(modifier = Modifier.align(Alignment.Center), painter = painterResource(id = R.drawable.logo), contentDescription = stringResource(R.string.app_logo) )
+        Image(modifier = Modifier
+            .align(Alignment.Center)
+            .rotate(degree), painter = painterResource(id = R.drawable.logo), contentDescription = stringResource(R.string.app_logo) )
     }
     }
 }
 @Composable
 @Preview
 fun SplashPreview(){
-    Splash()
+    Splash(0f)
 }
