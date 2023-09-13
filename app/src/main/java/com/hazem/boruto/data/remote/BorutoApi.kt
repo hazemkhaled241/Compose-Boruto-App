9f0c0512db545b31d9447377735316a067b33c42
hazem khaled <hazemk778@gmail.com>
2023-09-14 00:52:07 +0300

    define the api interface

---
+++ app/src/main/java/com/hazem/boruto/data/remote/BorutoApi.kt
@@ -0,0 +1,17 @@
+package com.hazem.boruto.data.remote
+
+import com.hazem.boruto.domain.model.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface BorutoApi {
+    @GET("boruto/heroes")
+    suspend fun getAllHeroes(
+        @Query("page") page: Int = 1
+    ): ApiResponse
+
+    @GET("boruto/heroes/search")
+    suspend fun searchForHeroes(
+        @Query("name") name:String
+    ): ApiResponse
+}
