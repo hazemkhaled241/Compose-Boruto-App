011b290e98f1039a68e62626301c9ea700c918d2
hazem khaled <hazemk778@gmail.com>
2023-09-13 21:20:32 +0300

    make the start destination splash screen

--- app/src/main/java/com/hazem/boruto/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/hazem/boruto/presentation/navigation/NavGraph.kt
@@ -6,17 +6,20 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.hazem.boruto.presentation.screens.home.HomeScreen
 import com.hazem.boruto.presentation.screens.splash.SplashScreen
 import com.hazem.boruto.presentation.screens.welcome.WelcomeScreen
 import com.hazem.boruto.utils.Constants.DETAILS_ARGUMENT_KEY
 
 @Composable
 fun SetUpNavGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = Screen.Welcome.route){
+    NavHost(navController = navController, startDestination = Screen.Splash.route){
         composable(route=Screen.Splash.route){
-            SplashScreen(navController)
+            SplashScreen(navController = navController)
         }
-        composable(route=Screen.Home.route){}
+        composable(route=Screen.Home.route){
+            HomeScreen()
+        }
         composable(route=Screen.Details.route, arguments = listOf(navArgument(DETAILS_ARGUMENT_KEY){
             type= NavType.IntType
         })){
