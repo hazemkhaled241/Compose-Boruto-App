30387eb28059c7cc995fde69527073efeca69e19
hazem khaled <hazemk778@gmail.com>
2023-09-15 20:47:39 +0300

    provide BorutoApi

---
+++ app/src/main/java/com/hazem/boruto/di/RemoteModule.kt
@@ -0,0 +1,37 @@
+package com.hazem.boruto.di
+
+import com.hazem.boruto.data.remote.BorutoApi
+import com.hazem.boruto.utils.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteModule {
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder().readTimeout(
+            15, TimeUnit.MINUTES)
+            .connectTimeout(15,TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideBorutoApi(okHttpClient: OkHttpClient): BorutoApi {
+        return Retrofit
+            .Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(BorutoApi::class.java)
+    }
+}
