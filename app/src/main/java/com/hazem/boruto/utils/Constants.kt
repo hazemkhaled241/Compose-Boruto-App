f5de726fb5f94378a24e4442631e51b4b2dae9c4
hazem khaled <hazemk778@gmail.com>
2023-09-15 20:47:06 +0300

    add new constants

--- app/src/main/java/com/hazem/boruto/utils/Constants.kt
+++ app/src/main/java/com/hazem/boruto/utils/Constants.kt
@@ -8,4 +8,6 @@ object Constants {
     const val ON_BOARDING_SCREEN_COUNT = 3
     const val PREFERENCES_NAME = "boruto_preferences"
     const val PREFERENCES_KEY = "on_boarding_completed"
+    const val BASE_URL = "http://10.0.2.2:8080"
+
 }
