03dcbb45099a451d9789e73c7591b6a05881a2cd
hazem khaled <hazemk778@gmail.com>
2023-09-16 17:19:27 +0300

    add new constants

--- app/src/main/java/com/hazem/boruto/utils/Constants.kt
+++ app/src/main/java/com/hazem/boruto/utils/Constants.kt
@@ -6,6 +6,7 @@ object Constants {
     const val HERO_REMOTE_KEY_DATA_BASE_TABLE = "hero_remote_key_table"
     const val HERO_DATA_BASE = "hero_database"
     const val ON_BOARDING_SCREEN_COUNT = 3
+    const val ITEMS_PER_PAGE = 3
     const val PREFERENCES_NAME = "boruto_preferences"
     const val PREFERENCES_KEY = "on_boarding_completed"
     const val BASE_URL = "http://10.0.2.2:8080"
